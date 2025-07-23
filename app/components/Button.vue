0c1f7ebe5d54c47921351dda8121cf6b823abbf6
k33n8nc <robbert.stapel@live.nl>
2025-07-23 13:11:41 +0200

    restore btn color

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
       type="button"
-      class="bg-primary rounded px-3 py-2 cursor-pointer text-white text-sm inline-flex items-center"
+      class="bg-emerald-500 rounded px-3 py-2 cursor-pointer text-white text-sm inline-flex items-center"
   >
     <slot />
   </button>
