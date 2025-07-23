61a60d0117e8810fac6af2a0b881cb158c37907c
k33n8nc <robbert.stapel@live.nl>
2025-07-23 04:11:41 +0200

    Edit default template.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <header class="bg-gray-200 h-14 fixed top-0 w-full z-10">
-      <nav class="container mx-auto h-full flex items-center px-4">
-        <h1 class="font-bold">Nuxt Starter</h1>
+      <nav class="container mx-auto h-full flex flex-col justify-center px-4">
+        <h1 class="font-bold">Nuxt 4 + Tawilwind 4 Starter</h1>
+        <small>See readme and package.json for more info.</small>
       </nav>
     </header>
 
@@ -12,10 +13,10 @@
 
     <footer class="bg-gray-200 h-14 fixed bottom-0 w-full">
       <div class="container mx-auto h-full flex items-center px-4">
-        <span>© {{ new Date().getFullYear() }}</span>
+        <p class="text-sm text-gray-500">
+          free to copy © <a href="https://cybernetix.nl" target="_blank">cybernetix {{ new Date().getFullYear() }}</a>
+        </p>
       </div>
     </footer>
   </div>
 </template>
-<script setup lang="ts">
-</script>
